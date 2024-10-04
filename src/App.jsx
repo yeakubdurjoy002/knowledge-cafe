9bb800d051ef9b30618801af7c04f5cb15459595
YeakubDurjoy001 <yeakubdurjoy002>
2024-10-04 12:03:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Blogs from "./Components/blogs/Blogs";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div>
+      <h1 className="text-4xl text-left font-bold m-7">Knowledge Cafe</h1>
+      <hr />
+    </div>
+  );
+}
+
+export default Header;
